1ceef863d527bf65223e78a1ad52ff38c10b06eb
MihajlovicSS <mihajlovicsinisa1456780@gmail.com>
2023-04-21 16:12:16 +0300

    First version

---
+++ calc/calculator.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const add = require('./add')
+const div = require('./divide')
+const mult = require('./multiply')
+const sub = require('./subtract')
+
+module.exports = {
+    add,
+    sub,
+    mult,
+    div
+}
